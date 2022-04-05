71f94b79eca8d4de4552d8e8b31e0669c3eed078
Matt Enterline <matthew.j.enterline@gmail.com>
2022-04-04 21:36:15 -0400

    Removed extra filed for clean start

--- menterline-f1-analysis/src/index.tsx
+++ menterline-f1-analysis/src/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <span>THIS IS A DEPLOYMENT TEST</span>
+    <span>THIS IS A ANOTHER DEPLOYMENT TEST</span>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
