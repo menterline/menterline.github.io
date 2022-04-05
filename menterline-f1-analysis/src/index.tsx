df47c627433cb1ab86123424f56b00914e39b754
Matt Enterline <matthew.j.enterline@gmail.com>
2022-04-04 21:10:00 -0400

    deployment test

--- menterline-f1-analysis/src/index.tsx
+++ menterline-f1-analysis/src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <span>THIS IS A DEPLOYMENT TEST</span>
   </React.StrictMode>,
   document.getElementById('root')
 );
